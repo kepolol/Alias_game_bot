1a7ff8e3bc62f263ae517e7ce1e9600fe172a517
kepolol <kepolol@gmail.com>
2019-12-04 12:40:21 +0300

    add services

--- app/main.py
+++ app/main.py
@@ -7,6 +7,17 @@ from flask import Blueprint, render_template, request, redirect
 main = Blueprint('main', __name__)
 
 
-@main.route('/')
-def index():
-    return 'OK'
+@main.route('/explain')
+def explain():
+    print(request.args)
+    word = request.args.get('word')
+    n_words = int(request.args.get('n_words'))
+
+    return json.dumps(player.explain(word=word, n_words=n_words))
+
+
+@main.route('/guess')
+def guess():
+    words = request.args.getlist('words')
+    n_words = int(request.args.get('n_words'))
+    return json.dumps(player.guess(words=words, n_words=n_words))
