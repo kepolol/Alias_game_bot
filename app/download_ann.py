ac3b6e28cc1b0d8ccf03edd80ff067a3e80d26e7
kepolol <kepolol@gmail.com>
2019-12-14 10:52:45 +0300

    fix some things

--- app/download_ann.py
+++ app/download_ann.py
@@ -2,4 +2,6 @@ import yadisk
 
 
 y = yadisk.YaDisk(token='AgAAAAA7LnbJAAYCAyDjyqmIDUXeoGtgE9eDzvM')
-y.download('data/', 'data/')
+y.download('data/word2vec.model', 'data/word2vec.model')
+y.download('data/word2vec_idx.ann', 'data/word2vec_idx.ann')
+y.download('data/word2vec_idx.ann.d', 'data/word2vec_idx.ann.d')
